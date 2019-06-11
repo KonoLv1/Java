bddaa168342dc8b4280184f1666e8f4125dc2264
Konolv1 <konolevel1@gmail.com>
2019-06-11 10:40:25 +0900

    Study File

---
+++ Project/Beginner/Study/Day1/Study3.java
@@ -0,0 +1,11 @@
+// Study3 Junichi Koyama
+// •Ï”‚Æ’è”‚ÆŒvZ
+
+public class Study3{
+	public static void main(String[]args){
+		int hensuu = 10;
+		final int teisuu = 15;
+		hensuu = hensuu + teisuu;
+		System.out.println(hensuu);
+	}
+}
