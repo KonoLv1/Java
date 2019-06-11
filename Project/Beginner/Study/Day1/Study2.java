96141a677e4fde4ef4f6b34e5d6ad880d3507b2d
Konolv1 <konolevel1@gmail.com>
2019-06-11 10:33:57 +0900

    Update Day1

---
+++ Project/Beginner/Study/Day1/Study2.java
@@ -0,0 +1,10 @@
+// Study2 Junichi Koyama
+// 様々な出力と改行の有無
+
+public class Study2{
+	public static void main(String[]arhs){
+		System.out.print("ABC");
+		System.out.println("ABC");
+		System.out.println(114514);
+	}
+}
