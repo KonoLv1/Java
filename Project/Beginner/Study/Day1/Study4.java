527043376bed4bc23c342f7f65f9cad91bbb6d01
Konolv1 <konolevel1@gmail.com>
2019-06-11 10:50:48 +0900

    Add Study4 - 文字連結の勉強

---
+++ Project/Beginner/Study/Day1/Study4.java
@@ -0,0 +1,12 @@
+// Study4 Junichi Koyama
+// •¶š—ñ˜AŒ‹
+
+public class Study4{
+	public static void main(String[]args){
+		String textA = "‡";
+		String textB = "‘Ì";
+		int no1 = 32;
+		int no2 = 1;
+		System.out.print(no1 * 10 + no2 + textA + textB);
+	}
+}
