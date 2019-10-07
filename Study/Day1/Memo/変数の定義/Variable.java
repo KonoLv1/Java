b957c21f8dfba78b03e636ce25acbd55cfae578f
Konolv1 <konolevel1@gmail.com>
2019-10-07 10:34:45 +0900

    Javaに関するメモ

---
+++ Study/Day1/Memo/変数の定義/Variable.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[]args){
+        int number1 = 10;
+        int number2 = 10;
+        int ans;
+        ans = number1 + number2;
+        System.out.println(ans);
+    }
+}
---
+++ Study/Day1/Memo/文字の出力/Text4.java
@@ -0,0 +1,5 @@
+class Main4{
+    public static void main(String[]args){
+        System.out.println(2019);
+    }
+}
